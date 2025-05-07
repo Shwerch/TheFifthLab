b1469285469c3e01799617e3086fe9319e51bd32
Ilia <ilia06popov@gmail.com>
2025-05-08 02:04:40 +0700

    Work in progress

--- Task1/library.h
+++ Task1/library.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <iostream>
 #include <map>
 #include <set>
@@ -9,3 +10,9 @@
 #define RACKS 10
 #define SECTIONS 7
 #define SHELFS 4
+
+#define LIMIT(min, value, max) (((value) >= (min)) && ((value) <= (max)))
+
+enum class AddProduct { INCORRECT_INPUT_DATA, CELL_IS_OCCUPIED_BY_ANOTHER_PRODUCT, NOT_ENOUGH_SPACE_IN_THE_CELL, ADDED_EXISTED_PROSUCT_SUCCESS, ADDED_NEW_PROSUCT_SUCCESS };
+
+enum class RemoveProduct { INCORRECT_INPUT_DATA, CELL_IS_OCCUPIED_BY_ANOTHER_PRODUCT, COMPLETE_REMOVAL_PRODUCT_SUCCESS, REMOVAL_PRODUCT_SUCCESS, NOT_FOUND_PRODUCT };
--- Task1/main.cpp
+++ Task1/main.cpp
@@ -1,32 +1,61 @@
 #include "library.h"
 
-using namespace std;
+std::map<std::array<int, 4>, std::pair<int, int>> storage;
+std::map<int, std::string> productNames;
 
-// Зона хранения - стеллажи
-map<char, set<int>> zones = {{'A', {}}};
-// Стеллаж - вертикальные секции
-map<int, set<int>> racks = {};
-// Вертикальная секция - номера полок
-map<int, set<int>> sections = {};
-// Полка - информация о товаре: ID и количество
-map<int, pair<int, int>> shelfs = {};
-// Название товара - ID товара
-map<string, int> productIDs = {};
+AddProduct add(int productID, int count, int zone, int rack, int section, int shelf) {
+	if (!LIMIT(1, zone, ZONEZ) || !LIMIT(1, rack, RACKS) || !LIMIT(1, section, SECTIONS) ||
+		!LIMIT(1, shelf, SHELFS) || !LIMIT(1, count, 10) ||
+		productNames.find(productID) == productNames.end())
+		return AddProduct::INCORRECT_INPUT_DATA;
+	std::array<int, 4> array = {static_cast<int>(zone), rack, section, shelf};
+	for (std::pair<std::array<int, 4>, std::pair<int, int>> element : storage) {
+		if (element.first != array)
+			continue;
+		else if (element.second.first != productID)
+			return AddProduct::CELL_IS_OCCUPIED_BY_ANOTHER_PRODUCT;
+		else if (element.second.second + count > 10)
+			return AddProduct::NOT_ENOUGH_SPACE_IN_THE_CELL;
+		element.second.second += count;
+		return AddProduct::ADDED_EXISTED_PROSUCT_SUCCESS;
+	}
+	std::pair<int, int> pair = {productID, count};
+	storage.insert({array, pair});
+	return AddProduct::ADDED_NEW_PROSUCT_SUCCESS;
+}
 
-// Щас можно вставить полки 987 17 616 26 266 и их будет 5, а надо по порядку 1 2 3 4 5
-bool add(int productID, int count, char zone, int rack, int section, int shelf) {
-	zones.at('A').insert(rack); // В зоне A есть стеллаж под номером rack
-	if (zones.at('A').size() > RACKS)
-		return false;
-	racks.at(rack).insert(section); // В стеллаже rack есть секция под номером section
-	if (racks.at(rack).size() > SECTIONS)
-		return false;
-	sections.at(section).insert(shelf); // В секции section есть полка под номером shelf
-	if (racks.at(rack).size() > SECTIONS)
-		return false;
-	pair<int, int> myPair = {1, 2};
-	shelfs.at(shelf).swap(myPair);
-	return true;
+RemoveProduct remove(int productID, int count, int zone, int rack, int section, int shelf) {
+	if (!LIMIT(1, zone, ZONEZ) || !LIMIT(1, rack, RACKS) || !LIMIT(1, section, SECTIONS) ||
+		!LIMIT(1, shelf, SHELFS) || !LIMIT(1, count, 10) ||
+		productNames.find(productID) == productNames.end())
+		return RemoveProduct::INCORRECT_INPUT_DATA;
+	std::array<int, 4> array = {zone, rack, section, shelf};
+	for (std::pair<std::array<int, 4>, std::pair<int, int>> element : storage) {
+		if (element.first != array)
+			continue;
+		else if (element.second.first != productID)
+			return RemoveProduct::CELL_IS_OCCUPIED_BY_ANOTHER_PRODUCT;
+		element.second.second -= count;
+		if (element.second.second <= 0) {
+			storage.erase(array);
+			return RemoveProduct::COMPLETE_REMOVAL_PRODUCT_SUCCESS;
+		}
+		return RemoveProduct::REMOVAL_PRODUCT_SUCCESS;
+	}
+	return RemoveProduct::NOT_FOUND_PRODUCT;
+}
+
+void info() {
+	for (int zone = 1; zone <= ZONEZ; zone++) {
+		for (int rack = 1; rack < RACKS; rack++) {
+			for (int section = 1; section < SECTIONS; section++) {
+				for (int shelf = 1; shelf < SHELFS; shelf++) {
+					std::cout << static_cast<char>(zone + static_cast<int>('A') - 1) << rack
+							  << section;
+				}
+			}
+		}
+	}
 }
 
 int main() { return 0; }
